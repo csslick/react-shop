423292ab38fb0dbc367a963b62b80632c9d4491e
csslick <digitone@naver.com>
2021-08-19 22:52:08 +0900

    product list

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable */
+import { useState } from 'react';
+import { Navbar, Container, Nav, Button} from "react-bootstrap";
+import "./App.css";
+import { Data } from './data';
 
 function App() {
+
+  const [items, setItems] = useState(Data);
+  console.log(Data);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar bg="light" expand="lg">
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ml-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#product">Product</Nav.Link>
+            <Nav.Link href="#contact">Contact</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+
+      <section className="title">
+        <Container>
+          <h2>Shop</h2>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum enim esse id distinctio quaerat illo facilis error possimus, dolore quasi ducimus sunt vero maiores itaque sit cumque temporibus saepe. Eius?</p>
+          <Button variant="primary">Primary</Button>
+        </Container>  
+      </section>
+
+      <section id="product" className="container product">
+        <Items data={items} />
+      </section>
+      
     </div>
   );
+}
+
+function Items({data}) {
+
+  return (
+    <div className="row">
+      {
+        data.map((item, i) => {
+          return (
+            <div className="col-md-4" key={i}>
+              <img src={item.image} width="100%" />
+              <h4>{item.title}</h4>
+              <p>{item.content}</p>
+              <p>{item.price.toLocaleString()}원</p>
+            </div>
+          )
+        })
+      }
+    </div>
+  )
 }
 
 export default App;
