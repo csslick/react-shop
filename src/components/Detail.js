78db05730ed4ed7271e3cfb5fca3a38412aff8f2
csslick <digitone@naver.com>
2021-08-22 23:01:18 +0900

    useEffect

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -1,32 +1,48 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
 import { useHistory, useParams } from 'react-router-dom'
-// import { useEffect } from 'react';
 import styled from 'styled-components'; // 오류시 4 또는 5 버전 사용
 // CSS 모듈 타입 선언(해당 컴퍼넌트 전용 CSS)
 import styles from './css/Detail.module.scss';
 
 // 스타일드 변수는 함수 밖에 선언하세요
 const StyledSection = styled.section`
-  border: 1px solid rgba(0,0,0,0.1);
   padding: 20px;
   margin-top: 20px;
   margin-bottom: 20px;
-  border-radius: 6px;
 `;
 
 export default function Detail({data}) {
 
   const history = useHistory();
   const { id } = useParams();
+  const [showAlert, setShowAlert] = useState(true);
 
   // param과 제품 id가 일치하는 제품 정보를 가져온다.
   const newData = data.find(item => {
       return item.id == id;  
   })
- 
+
+  useEffect(() => {
+
+    console.log('Detail mounted');
+    setTimeout(function(){
+      setShowAlert(false);
+    }, 2000);
+
+    // unmount 할 때 호출되는 코드
+    return () => {
+      console.log('Detail unmounted');
+    }
+
+  }, [])
+  
   return (
     <StyledSection className={styles.container}>
       <h2 className="text-center">제품상세정보</h2>
+      {
+        showAlert && 
+        <p className={styles.alert}>신제품이 입고되었습니다.</p>
+      }
       <div className="row">
         <div className="col-md-6">
           <img src={newData.image} alt={id} width="100%" />
