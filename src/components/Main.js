72352609000189d6706f9110320d26ad4d789db2
csslick <digitone@naver.com>
2021-08-24 00:02:57 +0900

    ajax1 axios

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,12 @@
 import React from 'react'
 import Item from './Item'
+import axios from 'axios';
+
 
 export default function Main({data}) {
+
+  // fetch()는 JSON을 Objec로 반환해주지 않아서 axios 사용함
+
   return (
     <section id="product" className="container product">
       <div className="row">
@@ -11,6 +16,17 @@ export default function Main({data}) {
           })
         }
       </div>
+      <button 
+        className="btn btn-primary mt-4"
+        onClick={()=>{
+          axios.get('https://raw.githubusercontent.com/csslick/react-shop/main/data2.json')
+          .then(res=>{
+            console.log(res.data)
+          })  
+          .catch(err=>{
+            console.log(err);
+          })
+        }}>더보기</button>
   </section>
   )
 }
